f0e5660c5f0baf86921ee18f726347b737a2603c
JoseAlvarez9212 <44510292+JoseAlvarez9212@users.noreply.github.com>
2018-10-26 17:01:16 -0500

    Model Product start

---
+++ Sales.Common/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sales.Common.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        public DateTime PublishOn { get; set; }
+    }
+}
