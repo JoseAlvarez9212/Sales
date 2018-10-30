05387d24bbf9279e644968049ae29be956e16e59
JoseAlvarez9212 <44510292+JoseAlvarez9212@users.noreply.github.com>
2018-10-30 07:23:03 -0600

    27

--- Sales.Common/Models/Product.cs
+++ Sales.Common/Models/Product.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sales.Common.Models
+﻿namespace Sales.Common.Models
 {
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
     public class Product
     {
+        [Key]
         public int ProductId { get; set; }
 
+        [Required]
         public string Description { get; set; }
 
         public decimal Price { get; set; }
---
+++ Sales/Sales/ViewModels/ProductsViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Sales.Common.Models;
+using Sales.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace Sales.ViewModels
+{
+    public class ProductsViewModel : BaseViewModel
+    {
+        private ApiService apiService;
+
+        private ObservableCollection<Product> products;
+
+        public ObservableCollection<Product> Products
+        {
+            get { return this.products; }
+            set { this.SetValue(ref this.products, value); }
+        }
+
+        public ProductsViewModel()
+        {
+            this.apiService = new ApiService();
+            this.LoadProducts();
+        }
+
+        private async void LoadProducts()
+        {
+            var response = await apiService.GetList<Product>("https://salesapiservice.azurewebsites.net", "/api","/products");
+            if (!response.IsSuccess)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error",response.Message,"Acept");
+                return;
+            }
+            var list = (List<Product>)response.Result;
+            this.Products = new ObservableCollection<Product>(list);
+        }
+    }
+}
